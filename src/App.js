18527285b77b1e95b80cbff4533d471b719a8436
Belal Safy <belal.aglan2@gmail.com>
2021-10-11 00:49:55 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          El SAFY is coming
         </p>
+
+        <img src="https://thumbs.gfycat.com/UnsightlyDirectJunco-size_restricted.gif" className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/belal.aglan2"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn bs
+          contact us
         </a>
       </header>
     </div>
